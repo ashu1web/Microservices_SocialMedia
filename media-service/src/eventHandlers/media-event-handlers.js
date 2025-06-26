d383bd11cf011371d3a338d4bf1279189021914c
ashutosh sharma <ashusharmads.18@gmail.com>
2025-06-26 17:57:00 +0530

    completed logic

--- media-service/src/eventHandlers/media-event-handlers.js
+++ media-service/src/eventHandlers/media-event-handlers.js
@@ -1,6 +1,26 @@
-const handlePostDeleted=async(event)=>{
-    console.log(event,"eventeventevent")
-}
+const Media = require("../models/Media");
+const { deleteMediaFromCloudinary } = require("../utils/cloudinary");
+const logger = require("../utils/logger");
 
+const handlePostDeleted = async (event) => {
+  console.log(event, "eventeventevent");
+  const { postId, mediaIds } = event;
+  try {
+    const mediaToDelete = await Media.find({ _id: { $in: mediaIds } });
 
-module.exports={handlePostDeleted}
+    for (const media of mediaToDelete) {
+      await deleteMediaFromCloudinary(media.publicId);
+      await Media.findByIdAndDelete(media._id);
+
+      logger.info(
+        `Deleted medua ${media._id} associated with this deleted post ${postId}`
+      );
+    }
+
+    logger.info(`Processed deletion of media for post id ${postId}`);
+  } catch (e) {
+    logger.error(e, "Error occured while media deletion");
+  }
+};
+
+module.exports = { handlePostDeleted };
